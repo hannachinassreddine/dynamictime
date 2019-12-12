cfe47c23445dd48a685a0693e8893142a602f419
hannachinassreddine <hannachi.nassreddine@gmail.com>
2019-12-12 13:51:52 +0100

    DynamicTyme first up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Dynamic from './dynamic/dynamic'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Dynamic ms={4600000}/>
     </div>
   );
 }
